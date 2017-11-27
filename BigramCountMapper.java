14640b470545d55d281f88337de3dd70765f16c5
raboin09 <raboin09@gmail.com>
2017-11-27 13:08:23 -0600

    Began implementation

--- BigramCountMapper.java
+++ BigramCountMapper.java
@@ -1,5 +1,7 @@
+
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.lang.StringBuilder;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -7,6 +9,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class BigramCountMapper extends Mapper<LongWritable, Text, BigramWritable, IntWritable> {
+
     private static final IntWritable ONE = new IntWritable(1);
     private static final BigramWritable BIGRAM = new BigramWritable();
 
@@ -14,14 +17,50 @@ public class BigramCountMapper extends Mapper<LongWritable, Text, BigramWritable
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         String prev = null;
-            StringTokenizer itr = new StringTokenizer(line);
-            while (itr.hasMoreTokens()) {
-                String cur = itr.nextToken();
-            if (prev != null) {
-                BIGRAM.set(new Text(prev),new Text(cur));
+        StringTokenizer itr = new StringTokenizer(line);
+        String[] arr = new String[itr.countTokens()];
+        for(int i=0; i<itr.countTokens(); i++){
+            arr[i] = itr.nextToken().toString();
+        }
+        BIGRAM.set(new Text("arr length"), new Text(String.valueOf(arr.length)));
+        context.write(BIGRAM, ONE);
+        String currentPerson = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            String cur = arr[i];
+            StringBuilder comFriends = new StringBuilder();
+            comFriends.append("{");
+            if (prev != null && cur != null) {
+                if (Integer.parseInt(currentPerson) < Integer.parseInt(cur)) {
+                    for(int j=0; j<arr.length; j++){
+                        if(j!= i && j != Integer.parseInt(currentPerson)){
+                            comFriends.append(arr[j]); 
+                        }
+                    }
+                    comFriends.append("}");
+                    String valueAdd = comFriends.toString();
+                    BIGRAM.set(new Text("(" + currentPerson + "," + cur + ")"), new Text(valueAdd));
+                } else if (Integer.parseInt(currentPerson) > Integer.parseInt(cur)) {
+                    for(int j=0; j<arr.length; j++){
+                        if(j!= i && j != Integer.parseInt(currentPerson)){
+                            comFriends.append(arr[j]); 
+                        }
+                    }
+                    comFriends.append("}");
+                    String valueAdd = comFriends.toString();
+                    BIGRAM.set(new Text("(" + cur + "," + currentPerson + ")"), new Text(valueAdd));
+                }
                 context.write(BIGRAM, ONE);
             }
             prev = cur;
         }
-    }   
+
+        /*while (itr.hasMoreTokens()) {
+            String cur = itr.nextToken();
+            if (prev != null) {
+                BIGRAM.set(new Text(prev), new Text(cur));
+                context.write(BIGRAM, ONE);
+            }
+            prev = cur;
+        }*/
+    }
 }
