13d98239196aa330f416757a5ead7197f5eaa12a
raboin09 <raboin09@gmail.com>
2017-11-27 16:26:14 -0600

    added arr[0] check

--- BigramCountMapper.java
+++ BigramCountMapper.java
@@ -22,7 +22,7 @@ public class BigramCountMapper extends Mapper<LongWritable, Text, BigramWritable
         for(int i=0; i<itr.countTokens(); i++){
             arr[i] = itr.nextToken().toString();
         }
-        BIGRAM.set(new Text("arr length"), new Text(String.valueOf(arr.length)));
+        BIGRAM.set(new Text("arr[0]"), new Text(arr[0]));
         context.write(BIGRAM, ONE);
         String currentPerson = arr[0];
         for (int i = 0; i < arr.length; i++) {
